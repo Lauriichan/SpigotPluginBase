28536b4432530231e34955de9958f05789da5efe
Lauriichan <lauriichan@playuniverse.org>
2024-12-31 00:34:50 +0100

    Add more sending methods

--- src/main/java/me/lauriichan/minecraft/pluginbase/message/component/ISendable.java
+++ src/main/java/me/lauriichan/minecraft/pluginbase/message/component/ISendable.java
@@ -10,7 +10,7 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.BaseComponent;
 
 public interface ISendable {
-    
+
     default void send(final Actor<?> actor) {
         final Actor<CommandSender> sender = actor.as(CommandSender.class);
         if (!actor.isValid()) {
@@ -50,10 +50,49 @@ public interface ISendable {
         }
     }
 
-    default void sendBroadcast() {
-        Bukkit.getServer().spigot().broadcast(buildComponentArray());
+    default void sendBroadcast(final World world, final String permission) {
+        sendBroadcast(world, ChatMessageType.CHAT, permission, true);
     }
-    
+
+    default void sendBroadcast(final World world, final ChatMessageType type, final String permission) {
+        sendBroadcast(world, type, permission, true);
+    }
+
+    default void sendBroadcast(final World world, final String permission, final boolean permissionValue) {
+        sendBroadcast(world, ChatMessageType.CHAT, permission, permissionValue);
+    }
+
+    default void sendBroadcast(final World world, final ChatMessageType type, final String permission, final boolean permissionValue) {
+        final BaseComponent[] message = buildComponentArray();
+        for (final Player player : world.getPlayers()) {
+            if (player.hasPermission(permission) != permissionValue) {
+                continue;
+            }
+            player.spigot().sendMessage(type, message);
+        }
+    }
+
+    default void sendBroadcast() {
+        BaseComponent[] components = buildComponentArray();
+        Bukkit.getConsoleSender().spigot().sendMessage(components);
+        Bukkit.getServer().spigot().broadcast(components);
+    }
+
+    default void sendBroadcast(String permission) {
+        sendBroadcast(permission, true);
+    }
+
+    default void sendBroadcast(String permission, boolean permissionValue) {
+        BaseComponent[] components = buildComponentArray();
+        Bukkit.getConsoleSender().spigot().sendMessage(components);
+        for (final Player player : Bukkit.getOnlinePlayers()) {
+            if (player.hasPermission(permission) != permissionValue) {
+                continue;
+            }
+            player.spigot().sendMessage(components);
+        }
+    }
+
     BaseComponent[] buildComponentArray();
 
 }
